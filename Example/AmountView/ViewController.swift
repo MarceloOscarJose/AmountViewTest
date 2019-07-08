b5dea1ff68222b0a7551edd6b736d657453b74f8
Marcelo Oscar José <marcelo@MacBook-Pro-Marcelo.local>
2019-07-08 10:44:19 -0300

    Initial commit

---
+++ Example/AmountView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AmountView
+//
+//  Created by MarceloOscarJose on 07/08/2019.
+//  Copyright (c) 2019 MarceloOscarJose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
